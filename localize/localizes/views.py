842f3acd7e12a3d686018d6650f2ec3086e249b3
Dmitriy Shipilov <shipilenok1@gmail.com>
2023-01-24 05:20:15 +0300

    Add localise

--- localize/localizes/views.py
+++ localize/localizes/views.py
@@ -3,12 +3,11 @@ from django.http import HttpResponse
 from django.utils.translation import ugettext as _
 
 '''
-unworking render of templates, like this:'''
+unworking render of templates, like this:
 def index(request):
     return render(request, 'localizes/index.html')
-
 '''
 
 def index(request):
     output = _('My Test to be translated')
-    return HttpResponse(output)'''
+    return HttpResponse(output)
